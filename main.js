03d0bcf06ae5d1f2a0081d296458b409ef400969
julien_ripet <ripetjulien@gmail.com>
2019-02-04 14:04:32 +0100

    Not sure where i left

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
+let nombreCookie = 0;
+let multiplier = 1;
+let additionnal = 0;  
+let score = document.getElementById("Score");
+let message = document.getElementById("messageUser");
+let palier = [1,200];
+let palierCourrant = 0;
+
 window.onload = function(){
+    score = document.getElementById("Score");
+    message = document.getElementById("messageUser");
+    score.textContent = nombreCookie;
+    message.textContent = "";
+    console.log(score.innerHTML);
 
-    let nombreCookie = 0;
-    let cookie = {
-        number : 0,
-        multiplier : 1,
-        additionnal : 0,    
-        show: function(){ 
-            document.getElementById("Score").textContent = this.number;
-        },
-        click: function(){
-            this.number = (1*this.multiplier)+ this.additionnal;
-            this.show();
-        },
-
-    }
-    let score = document.getElementById("Score");
-    cookie.show();
-    cookie.click();
 }
 
-cookie.click();
+let main = function(){   
+    let show = function(){ 
+            score.textContent = nombreCookie;
+        }
+    let click01 = function(){
+        nombreCookie =  nombreCookie + (1 *multiplier) + additionnal;
+        show();
+};
+
+    let score = document.getElementById("Score");
+    show();
+    click01();
+}
+
+let multiplication = function(){
+    console.log(palierCourrant);
+    if (nombreCookie > palier[palierCourrant]){
+        multiplier*=2;
+        nombreCookie -= palier[palierCourrant];
+        score.textContent = nombreCookie;
+        palierCourrant++;
+        message.textContent = "Multiplication achetée";
+
+    }
+    else {
+        if(palier.length = palierCourrant){
+            message.textContent = "Multiplication maximum atteinte!";
+        } else{
+            message.textContent = "Pas assez de cookies";
+        }
+    }
+}
