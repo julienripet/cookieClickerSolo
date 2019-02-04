3eaa2847bf4c53dda584e0cf1fd2d131ed110281
julien_ripet <ripetjulien@gmail.com>
2019-02-04 16:06:55 +0100

    Oh my god, so many changes and so few commits

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let nombreCookie = 0;
+let nombreClick = 0;
 let multiplier = 1;
 let additionnal = 0;  
+
 let score = document.getElementById("Score");
 let message = document.getElementById("messageUser");
-let palier = [1,200];
-let palierCourrant = 0;
+
+let palierMulti = [1,20,50,100,200,500];
+let palierCourrantMulti = 0;
+
+let palierAddiCout = [1,20,50,200,500];
+let palierAddi = [1,10,15,20,50];
+let palierCourrantAddi = 0;
 
 window.onload = function(){
     score = document.getElementById("Score");
@@ -16,6 +23,8 @@ window.onload = function(){
 }
 
 let main = function(){   
+    nombreClick++;
+    console.log(nombreClick);
     let show = function(){ 
             score.textContent = nombreCookie;
         }
@@ -30,20 +39,49 @@ let main = function(){
 }
 
 let multiplication = function(){
-    console.log(palierCourrant);
-    if (nombreCookie > palier[palierCourrant]){
+
+    console.log("Multiplication")
+    console.log(palierMulti)
+    console.log(palierCourrantMulti);
+
+    if (nombreCookie > palierMulti[palierCourrantMulti]){
         multiplier*=2;
-        nombreCookie -= palier[palierCourrant];
+        nombreCookie -= palierMulti[palierCourrantMulti];
         score.textContent = nombreCookie;
-        palierCourrant++;
+        palierCourrantMulti++;
         message.textContent = "Multiplication achetée";
 
     }
     else {
-        if(palier.length = palierCourrant){
+        if(palierMulti.length <= palierCourrantMulti){
             message.textContent = "Multiplication maximum atteinte!";
         } else{
             message.textContent = "Pas assez de cookies";
         }
     }
+}
+
+let addition = function(){
+
+    console.log("addition");
+    console.log(palierAddi);
+    console.log(palierAddiCout);
+    console.log(palierCourrantAddi);
+
+    if (nombreCookie > palierAddiCout[palierCourrantAddi]){
+        addition+= palierAddi[palierCourrantAddi];
+        nombreCookie -= palierAddiCout[palierCourrantAddi];
+        score.textContent = nombreCookie;
+        palierCourrantAddi++;
+        message.textContent = "Addition achetée";
+
+    }
+    else {
+        if(palierAddiCout.length = palierCourrantAddi){
+            message.textContent = "Addition maximum atteinte!";
+        } else{
+            message.textContent = "Pas assez de cookies";
+        }
+    }
+
 }
