08de91598b951696d1d4b6efe9f2cb84c1811e61
julien_ripet <ripetjulien@gmail.com>
2019-02-05 10:12:57 +0100

    Ajout de la possibilité de désactiver le bonus addition

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ let additionnal = 0; //Bonus achetable par le joueur, augmente le nombre de cook
 
 let score = document.getElementById("Score"); //Init de l'affichage du score
 let message = document.getElementById("messageUser");   //Init de l'affichage des messages d'info, ou d'erreur
+let btnAddition = document.getElementById("Addition"); // Init de la variable pour afficher ou non le bouton addition
 
 let palierMultiCout = [1,20,50,100,200,500];    //Cout de chaque multiplicateur, augmente en fonction de palierCourrantMulti
 let palierCourrantMulti = 0;    //Sert à se déplacer dans le tableau palierMultiCout, pour augmenter le prix des multiplicateurs après chaque achat
 
-
-let boolAddition = true;    //Booléen, permettant de désactiver le bonus addition, si non désiré. Simplement le mettre à false.
+//Bonus inspiré du cookieclicker du CERN, n'est pas obligatoire, peut être désactivé sans danger
+let boolAddition = true;   //Booléen, permettant de désactiver le bonus addition, si non désiré. Simplement le mettre à false.
 let palierAddiCout = [1,20,50,200,500];     //Cout de chaque bonus d'addition, augmente en fonction de palierCourrantAddi
 let palierAddi = [1,10,15,20,50];   //Bonus de cookies gagnés à chaque click en plus de la multiplication 
 let palierCourrantAddi = 0;     //Compteur, sers à se déplacer dans les tableaux palierAddi et palierAddiCout
@@ -20,10 +21,14 @@ let palierCourrantAddi = 0;     //Compteur, sers à se déplacer dans les tablea
 window.onload = function(){
     score = document.getElementById("Score");
     message = document.getElementById("messageUser");
+    let btnAddition = document.getElementById("Addition");
     score.textContent = nombreCookie;
     message.textContent = "";
     console.log(score.innerHTML);
-
+    if (!boolAddition){
+        console.log("Mode multiplication only")
+        btnAddition.parentNode.removeChild(btnAddition);
+    }
 }
 
 // Fonction principale, appelée dès que le Cookie est cliqué, ou quand l'auto-clicker s'active(à implémenter)
@@ -74,21 +79,21 @@ let addition = function(){
     console.log(palierAddi);
     console.log(palierAddiCout);
     console.log(palierCourrantAddi);
+    if(boolAddition){
+        if (nombreCookie >= palierAddiCout[palierCourrantAddi]){
+            additionnal = additionnal + palierAddi[palierCourrantAddi];
+            nombreCookie -= palierAddiCout[palierCourrantAddi];
+            score.textContent = nombreCookie;
+            palierCourrantAddi++;
+            message.textContent = "Addition achetée";
 
-    if (nombreCookie >= palierAddiCout[palierCourrantAddi]){
-        additionnal = additionnal + palierAddi[palierCourrantAddi];
-        nombreCookie -= palierAddiCout[palierCourrantAddi];
-        score.textContent = nombreCookie;
-        palierCourrantAddi++;
-        message.textContent = "Addition achetée";
-
-    }
-    else {
-        if(palierAddiCout.length <= palierCourrantAddi){
-            message.textContent = "Addition maximum atteinte!";
-        } else{
-            message.textContent = "Pas assez de cookies";
+        }
+        else {
+            if(palierAddiCout.length <= palierCourrantAddi){
+                message.textContent = "Addition maximum atteinte!";
+            } else{
+                message.textContent = "Pas assez de cookies";
+            }
         }
     }
-
 }
