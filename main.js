6f86127032096ee056100a4385099f2b24cef302
julien_ripet <ripetjulien@gmail.com>
2019-02-05 10:52:46 +0100

    I did it again ...

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ let nombreClick = 0;    //Compteur de click, ne sers à rien dans le programme e
 let multiplicateur = 1; //Bonus achetable par le joueur, multiplie le nombre de cookies par click, actuellement, se multiplie par 2 à chaque achat
 let additionnal = 0; //Bonus achetable par le joueur, augmente le nombre de cookies gagnés par click par un nombre, cfr palierAddi  
 
+
+//Variables d'affichages, Déclarées ici une premiere fois pour avoir un scope global
 let score = document.getElementById("Score"); //Init de l'affichage du score
 let message = document.getElementById("messageUser");   //Init de l'affichage des messages d'info, ou d'erreur
-let btnAddition = document.getElementById("Addition"); // Init de la variable pour afficher ou non le bouton addition
+let btnAddition = document.getElementsByClassName("Addition"); // Init de la variable pour afficher ou non le bouton addition
+let tableMulti = document.getElementById("affichageMultiplicateur");
+let tableAddi = document.getElementById("affichageAddition");
+let tableClicks = document.getElementById("affichageClicks");
 
 let palierMultiCout = [1,20,50,100,200,500];    //Cout de chaque multiplicateur, augmente en fonction de palierCourrantMulti
 let palierCourrantMulti = 0;    //Sert à se déplacer dans le tableau palierMultiCout, pour augmenter le prix des multiplicateurs après chaque achat
@@ -16,18 +21,34 @@ let palierAddiCout = [1,20,50,200,500];     //Cout de chaque bonus d'addition, a
 let palierAddi = [1,10,15,20,50];   //Bonus de cookies gagnés à chaque click en plus de la multiplication 
 let palierCourrantAddi = 0;     //Compteur, sers à se déplacer dans les tableaux palierAddi et palierAddiCout
 
+//Petite fonction trouvée sur Stackoverflow at https://stackoverflow.com/questions/4777077/removing-elements-by-class-name
+//Permet de faire disparaitre tous les éléments liés au bonus addition
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
 
 //Fonction onload, assigne les éléments html à leur variables
 window.onload = function(){
     score = document.getElementById("Score");
     message = document.getElementById("messageUser");
-    let btnAddition = document.getElementById("Addition");
+    btnAddition = document.getElementsByClassName("Addition");
+    tableMulti = document.getElementById("affichageMultiplicateur");
+    tableAddi = document.getElementById("affichageAddition");
+    tableClicks = document.getElementById("affichageClicks");
+
     score.textContent = nombreCookie;
     message.textContent = "";
+    tableAddi.textContent = "+" + additionnal;
+    tableMulti.textContent = "X" + multiplicateur;
+    tableClicks.textContent = nombreClick;
     console.log(score.innerHTML);
+
     if (!boolAddition){
         console.log("Mode multiplication only")
-        btnAddition.parentNode.removeChild(btnAddition);
+        removeElementsByClass("Addition");
     }
 }
 
